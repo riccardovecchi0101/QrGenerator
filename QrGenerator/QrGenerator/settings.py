695a37d90e8c17f5e11e63ba2aa92521e8cf9b69
Riccardo Vecchi <riccardo.vecchi0101@gmail.com>
2024-07-29 18:42:47 +0200

    settings changings

--- QrGenerator/QrGenerator/settings.py
+++ QrGenerator/QrGenerator/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 from django.core.exceptions import ImproperlyConfigured
-from django.core.serializers import json
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
